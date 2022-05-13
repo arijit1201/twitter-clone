6e7d13fbe7b7b0e51ff5af1fd16b33017e10e6ec
arijit1201 <arijit1201@gmail.com>
2022-05-13 08:34:46 +0530

    added a TweetComponent

--- components/Feed/TweetComponent.tsx
+++ components/Feed/TweetComponent.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Tweet } from '../../typings'
+import React from 'react';
+import TimeAgo from 'react-timeago';
+import { Tweet } from '../../typings';
 
 interface Props{
   tweet: Tweet
@@ -7,7 +8,25 @@ interface Props{
 
 function TweetComponent({tweet} : Props) {
   return (
-    <div>Tweet</div>
+    <div className="flex flex-col border-y border-gray-100 p-5">
+    <div className="flex space-x-3">
+      <img className="h-10 w-10 rounded-full object-cover" src={tweet.profileImg} alt="" />
+
+      <div>
+        <div className="flex items-center space-x-1 ">
+          <p className="mr-1 font-bold">{tweet.username}</p>
+          <p className="hidden text-sm text-gray-500 sm:inline">@{tweet.username.replace(/\s+/g, '').toLowerCase()} .</p>
+          <TimeAgo className="text-sm text-gray-500"
+          date={tweet._createdAt}/>
+        </div>
+
+        <p className="pt-1">{tweet.text} </p>
+
+        {tweet.image && <img className="m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm" src={tweet.image} alt=""/>}
+
+      </div>
+    </div>
+    </div>
   )
 }
 
